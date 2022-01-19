12474633e82769f48c717a2969a3af73df5b62ef
Lahumerad77 <93986046+Lahumerad77@users.noreply.github.com>
2022-01-19 23:40:40 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, {Component, useState} from "react";
+import "./../styles/App.css";
+
+function App() {
+  const [show, setShow] = useState(false);
+  return (
+    <div id="main">
+      <button id="click" onClick={function(){
+        setShow(!show);
+      }}>
+        Toggle P
+      </button>
+    {show ? (
+        <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+    ): null}
+    </div>
+  );
+}
+
+
+export default App;
